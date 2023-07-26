c46dde34fcafd51c6d3b019ed867775a4a914936
Taras Makarchuk <2278452@gmail.com>
2023-07-26 20:03:24 +0300

    login form style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import styles from "./App.module.scss";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.parent}>
       <h1 className={styles.header}>Hello</h1>
-      <button className={styles.button}>Login</button>
+      <div>
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className={styles.button}>Login</button>
+      </div>
     </div>
   );
 }
